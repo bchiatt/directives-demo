80febed01f412f6a795e797a0ed49e8e20f5fc5d
Brian Hiatt <brian.hiatt41@gmail.com>
2014-09-16 12:45:11 -0500

    geolocate added

---
+++ client/components/directives/bc-locate/bc-locate.js
@@ -0,0 +1,44 @@
+/* jshintrc camelcase: true*/
+(function(){
+  'use strict';
+
+  angular.module('bcLocateModule', [])
+  .factory('LocationService', ['$q', function($q){
+
+    function locate(){
+      var deferred = $q.defer(),
+          options  = {enableHighAccuracy: true, timeout: 10000, maximumAge: 0};
+
+      navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, options);
+
+      return deferred.promise;
+    }
+
+    return {locate:locate};
+  }])
+  .directive('bcLocate', [function(){
+    var o = {};
+
+    o.restrict    = 'A';
+    o.templateUrl = '/components/directives/bc-locate/bc-locate.html';
+    o.scope       = {};
+    o.link        = function(scope, element, attrs){
+                    };
+
+    o.controller  = ['$scope', 'LocationService', function($scope, LocationService){
+                      $scope.findMe = function(){
+                        LocationService.locate().then(success, error);
+                      };
+
+                      function success(pos){
+                        console.log(pos);
+                      }
+
+                      function error(err){
+                        console.log(err);
+                      }
+                    }];
+
+    return o;
+  }]);
+})();
